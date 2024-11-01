28deaffde6d695daa3719fb4b388eb017e3a5cef
Anowar Shajib <ajshajib@uchicago.edu>
2024-11-01 17:58:16 -0500

    Add installable package structure

---
+++ sl_forecast/cli.py
@@ -0,0 +1,16 @@
+"""Console script for sl_forecast."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for sl_forecast."""
+    click.echo("Replace this message by putting your code into "
+               "sl_forecast.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
