5b0b5ddc6adcec094929e2c2e469f492d8368d31
Anowar Shajib <ajshajib@uchicago.edu>
2024-11-18 12:11:45 -0700

    Add setup and teardown methods to TestSLForecast class

--- tests/test_sl_forecast.py
+++ tests/test_sl_forecast.py
@@ -8,8 +8,15 @@ import pytest
 class TestSLForecast:
     """Tests for `sl_forecast` package."""
 
-    @pytest.fixture
+    def setup_class(cls):
+        """Setup test fixtures, if any."""
+        pass
+
     def test_version(self):
         from sl_forecast import __version__
 
         assert __version__ is not None
+
+    def teardown_class(cls):
+        """Teardown test fixtures, if any."""
+        pass
