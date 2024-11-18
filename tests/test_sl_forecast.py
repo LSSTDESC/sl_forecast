95506a0f719f7e3cf98b81b1337e28e99a2e9b0a
Anowar Shajib <ajshajib@uchicago.edu>
2024-11-18 12:07:47 -0700

    Update package metadata and remove CLI script; add initial test structure

--- tests/test_sl_forecast.py
+++ tests/test_sl_forecast.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
 """Tests for `sl_forecast` package."""
+
+import pytest
+
+
+class TestSLForecast:
+    """Tests for `sl_forecast` package."""
+
+    @pytest.fixture
+    def test_version(self):
+        from sl_forecast import __version__
