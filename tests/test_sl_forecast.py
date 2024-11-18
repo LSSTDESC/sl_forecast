ba10daad57d85c0a9cd314a6dbd5cccc1a084545
Anowar Shajib <ajshajib@uchicago.edu>
2024-11-18 11:57:55 -0700

    Remove redundant test class and methods from test_sl_forecast.py

--- tests/test_sl_forecast.py
+++ tests/test_sl_forecast.py
@@ -1,33 +1,3 @@
 #!/usr/bin/env python
 
 """Tests for `sl_forecast` package."""
-
-
-import unittest
-from click.testing import CliRunner
-
-from sl_forecast import sl_forecast
-from sl_forecast import cli
-
-
-class TestSl_forecast(unittest.TestCase):
-    """Tests for `sl_forecast` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert "sl_forecast.cli.main" in result.output
-        help_result = runner.invoke(cli.main, ["--help"])
-        assert help_result.exit_code == 0
-        assert "--help  Show this message and exit." in help_result.output
